dcda0c397b008ce8116dff579a9b7eea61f2c4fe
jerin005 <jerinjbakthan@gmail.com>
2025-11-21 14:49:01 +0530

    Add files via upload

--- Program 10.cpp
+++ Program 10.cpp
@@ -1,33 +1,29 @@
 #include <stdio.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <unistd.h>
 #include <string.h>
 
-struct msg_buffer {
-    long msg_type;
-    char msg_text[100];
+struct Message {
+    int id;
+    char text[100];
 };
 
 int main() {
-    struct msg_buffer message;
-    key_t key;
-    int msgid;
+    struct Message msg;
 
-    key = ftok("progfile", 65);            
-    msgid = msgget(key, 0666 | IPC_CREAT); 
-    
-    if (fork() == 0) {
-        msgrcv(msgid, &message, sizeof(message), 1, 0);
-        printf("Child received: %s\n", message.msg_text);
-    } else {
-        message.msg_type = 1;
-        strcpy(message.msg_text, "Hello from Parent");
-        msgsnd(msgid, &message, sizeof(message), 0);
-        printf("Parent sent message.\n");
-        sleep(1);
-    }
-    msgctl(msgid, IPC_RMID, NULL); 
+    printf("Enter Message ID: ");
+    scanf("%d", &msg.id);
+
+    printf("Enter Message Text: ");
+    scanf("%s", msg.text);
+
+    printf("\n--- Message Queue Simulation ---\n");
+    printf("Message Sent to Queue\n");
+    printf("ID   : %d\n", msg.id);
+    printf("Text : %s\n", msg.text);
+
+    printf("\nMessage Received from Queue\n");
+    printf("ID   : %d\n", msg.id);
+    printf("Text : %s\n", msg.text);
+
     return 0;
 }
 
